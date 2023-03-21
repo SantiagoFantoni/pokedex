51ae0e426e41684fd59deed2dfc2a107a8c0586f
Santiago Fantoni <fantonisantiago@gmail.com>
2023-03-21 01:14:02 -0300

    Display random pokemon

--- src/components/PokeName.jsx
+++ src/components/PokeName.jsx
@@ -5,19 +5,27 @@ function PokeName() {
 	const [data, setData] = useState(null);
 
 	useEffect(() => {
+		const random = Math.ceil(Math.random() * 199);
 		const getData = async () => {
-			const response = await axios.get("https://pokeapi.co/api/v2/pokemon/3");
-			setData(response.data);
+			const response = await axios.get(
+				`https://pokeapi.co/api/v2/pokemon/${random}`
+			);
+			await setData(response.data);
 			console.log(data);
 		};
 		getData();
 	}, []);
 
-	return (
-		<>
-			<p>{data.name}</p>
-		</>
-	);
+	if (data != null)
+		return (
+			<>
+				<h1>{data.name.charAt(0).toUpperCase() + data.name.slice(1)}</h1>
+				<img src={data.sprites.front_default} alt='' />
+				<p>
+					{data.stats[0].stat.name}:{data.stats[0].base_stat}
+				</p>
+			</>
+		);
 }
 
 export default PokeName;
